02eaf046bb19a4f81d2cba9ea6ea85ae69d57d38
Iago-Ferreira-Silva <iagoferreira008@gmail.com>
2024-11-03 01:23:36 -0300

    Alteracoes no commit

---
+++ Project_FilmFrenzy/js/key.js
@@ -0,0 +1,4 @@
+// Insira aqui a chave da API que você recebeu em seu e-mail
+// link: https://api.themoviedb.org/3/movie/550?api_key=3330f16462f8795c970688ef5f8863cd
+
+key = "3330f16462f8795c970688ef5f8863cd";
--- Project_FilmFrenzy/key.js
+++
@@ -1,5 +0,0 @@
-// Insira aqui a chave da API que você recebeu em seu e-mail
-// link: https://www.omdbapi.com/?i=tt3896198&apikey=9efb20f5
-
-
-key = "9efb20f5";
